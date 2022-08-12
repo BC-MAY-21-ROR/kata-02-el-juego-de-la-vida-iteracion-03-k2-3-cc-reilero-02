13f1c2230f2aa8154dfe7920b0e973e953b949e6
Daniel Preciado <fpreciado3@ucol.mx>
2022-08-12 17:14:07 -0500

    add two create_table method test cases using rspec
    Co-authored-by: CZamora5 <czamora5@ucol.mx>
    Co-authored-by: HopeSaurus <hopexime@outlook.com>
    Co-authored-by: Dcastanedaan <davinsonbmx@gmail.com>

--- grid.rb
+++ grid.rb
@@ -1,12 +1,12 @@
 class Grid
-    attr_accessor :width, :heigth, :gridTable
-    def initialize(width,heigth)
+    attr_accessor :width, :height, :gridTable
+    def initialize(height, width)
         @width = width
-        @heigth = heigth
+        @height = height
     end
 
     def create_table
-        @gridTable = Array.new(@heigth){ Array.new(@width) {'.'} }
+        @gridTable = Array.new(@height){ Array.new(@width) {'.'} }
     end
    
 end
@@ -14,24 +14,34 @@ end
 class View_grid
 
     def show_table(grid)
-        for i in 0..grid.heigth-1 
+        for i in 0..grid.height-1 
             p grid.gridTable[i].join(' ')
         end
     end
 
 end
 
+describe Grid do
+    describe "#create_table" do
+        it "returns 3 sets of arrays containing three dots" do
+            expect(Grid.new(3, 3).create_table).to eq([[".",".","."], [".",".","."], [".",".","."]])
+        end
+        it "returns 6 sets of arrays containing two dots" do
+            expect(Grid.new(6, 2).create_table).to eq([[".","."], [".","."], [".","."], [".","."], [".","."], [".","."]])
+      end
+    end
+  end
+
 class Main
     def run
 
-        grid = Grid.new(3,3)
+        grid = Grid.new(3, 3)
         grid.create_table
 
         view_grid = View_grid.new()
         view_grid.show_table(grid)
     end
 end
-
 
 main = Main.new
 main.run
